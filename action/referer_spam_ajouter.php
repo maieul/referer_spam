c24b9ba073a0344c11f415a347680f327b6adbc8
bordun <jean-emmanuel.doucet@groolot.net>
2016-01-19 15:44:26 +0100

    Marquer un spam ne supprime plus le referer de la base -* Cela permet de conserver les données en cas d'erreur -* Un filtrage est executé au calcul de la liste des referer + tri par date d'ajout des spam

--- action/referer_spam_ajouter.php
+++ action/referer_spam_ajouter.php
@@ -1,21 +1,21 @@
 <?php
-    
+
     if (!defined("_ECRIRE_INC_VERSION")) {
 	    return;
     }
-    
+
     function action_referer_spam_ajouter($p,$arg) {
 	    include_spip('base/abstract_sql');
 
 	    $url = $_GET['url'];
 
-        if (sql_countsel("spip_referers", "referer LIKE '%$url%'")) 
-            sql_delete("spip_referers", "referer LIKE '%$url%'");
-            
-        if (sql_countsel("spip_referers_articles", "referer LIKE '%$url%'")) 
-            sql_delete("spip_referers_articles", "referer LIKE '%$url%'");
-            
-        if (!sql_countsel("spip_referer_spam", "referer LIKE '%$url%'")) 
+        // if (sql_countsel("spip_referers", "referer LIKE '%$url%'")) 
+        //     sql_delete("spip_referers", "referer LIKE '%$url%'");
+        //
+        // if (sql_countsel("spip_referers_articles", "referer LIKE '%$url%'"))
+        //     sql_delete("spip_referers_articles", "referer LIKE '%$url%'");
+
+        if (!sql_countsel("spip_referer_spam", "referer LIKE '%$url%'"))
             sql_insertq('spip_referer_spam',
                 array(
                     'date' => date('Y-m-d H:i:s'),
